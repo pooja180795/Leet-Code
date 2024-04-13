6f696fe044abdb7718314a319be26637d83bbd3d
Pooja Chothani <142337423+pooja180795@users.noreply.github.com>
2024-04-12 23:22:04 -0400

    Update 2)merge_array_in ascending.py

--- General_Easy_or_Medium_Progs/2)merge_array_in ascending.py
+++ General_Easy_or_Medium_Progs/2)merge_array_in ascending.py
@@ -1,8 +1,8 @@
-array1 = [1,2,3,0,0,0]
-array2 = [2,5,6]
-
-
-
-array1.extend(array2)
-array1.sort()
-print(array1)
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        while 0 in nums1 and n > 0:
+            nums1.remove(0)
+            n -= 1
+        nums1.extend(nums2)
+        nums1.sort()
+        
