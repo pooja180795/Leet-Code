48e4f414c63a5caf15ead7cb6b11caa59522db15
Pooja Chothani <142337423+pooja180795@users.noreply.github.com>
2024-04-16 00:08:00 -0400

    Update 3)ROMAN to INTEGER.py

--- General_Easy_or_Medium_Progs/3)ROMAN to INTEGER.py
+++ General_Easy_or_Medium_Progs/3)ROMAN to INTEGER.py
@@ -1,35 +1,59 @@
-# ROMAN to INTEGER
-r_n = 'XIV'
-if 'IV' in r_n:
-    r_n = r_n.replace("IV", "IIII")
-if 'IX' in r_n:
-    r_n = r_n.replace('IX', 'VIIII')
-if 'XL' in r_n:
-    r_n = r_n.replace('XL', 'XXXX')
-if 'XC' in r_n:
-    r_n = r_n.replace('XC', 'LXXXX')
-if 'IV' in r_n:
-    r_n = r_n.replace('CD', 'CCCC')
-if 'CM' in r_n:
-    r_n = r_n.replace('CM', 'DCCCC')
+'''
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 
-i_n = 0
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 
-for i in r_n:
-    if i == 'I':
-        i = 1
-    elif i == 'V':
-        i = 5
-    elif i == 'X':
-        i = 10
-    elif i == 'L':
-        i = 50
-    elif i == 'C':
-        i = 100
-    elif i == 'D':
-        i = 500
-    elif i == 'M':
-        i = 1000
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
 
-    i_n = i_n + i
-print(i_n)
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+
+'''
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        if 'IV' in s:
+            s = s.replace("IV", "IIII")
+        if 'IX' in s:
+            s = s.replace('IX', 'VIIII')
+        if 'XL' in s:
+            s = s.replace('XL', 'XXXX')
+        if 'XC' in s:
+            s = s.replace('XC', 'LXXXX')
+        if 'CD' in s:
+            s = s.replace('CD', 'CCCC')
+        if 'CM' in s:
+            s = s.replace('CM', 'DCCCC')
+
+        ans = 0
+
+        for i in s:
+            if i == 'I':
+                i = 1
+            elif i == 'V':
+                i = 5
+            elif i == 'X':
+                i = 10
+            elif i == 'L':
+                i = 50
+            elif i == 'C':
+                i = 100
+            elif i == 'D':
+                i = 500
+            elif i == 'M':
+                i = 1000
+
+            ans += i
+        return ans
+    
+sol_obj = Solution()
+print(sol_obj.romanToInt('MCDLXXVI'))
