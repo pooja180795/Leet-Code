e4cc0c550a976d601bfa0f8dcbc9ae77230157bf
Pooja Chothani <142337423+pooja180795@users.noreply.github.com>
2024-04-18 03:19:35 -0400

    Add files via upload

---
+++ April_Daily_Challenges/18)island_perimeter.py
@@ -0,0 +1,27 @@
+'''
+You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
+
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+
+The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+'''
+
+class Solution:
+    def islandPerimeter(self, grid) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        perimeter = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    perimeter += 4
+                    if r > 0 and grid[r-1][c] == 1:
+                        perimeter -= 2
+                    if c > 0 and grid[r][c-1] == 1:
+                        perimeter -= 2
+        return perimeter
+
+sol_obj = Solution()
+print(sol_obj.islandPerimeter([[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]))
