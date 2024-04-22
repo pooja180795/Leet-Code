d1003a7ff7af787cd2f0a579fb433273ac1bae06
Pooja Chothani <142337423+pooja180795@users.noreply.github.com>
2024-04-22 19:55:23 -0400

    Add files via upload

---
+++ General_Easy_or_Medium_Progs/32)is_subsequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        sp = tp = 0
+
+        while sp < len(s) and tp < len(t):
+            if s[sp] == t[tp]:
+                sp += 1
+            tp += 1
+        return sp == len(s)
+        
+sol_obj = Solution()
+print(sol_obj.isSubsequence("ab", "ababab"))
